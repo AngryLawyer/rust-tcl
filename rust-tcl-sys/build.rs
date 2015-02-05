30b7aabeae81d0b3702aa88531079e80ff9734da
Tony Aldridge <tony@dabapps.com>
2015-02-05 10:27:48 +0000

    Updated build.rs to use tcl8.5 for now

--- rust-tcl-sys/build.rs
+++ rust-tcl-sys/build.rs
@@ -5,13 +5,13 @@ extern crate "pkg-config" as pkg_config;
 fn main() {
     if std::os::getenv("CARGO_FEATURE_USE_PKGCONFIG").is_some() {
       if build_pkgconfig() { return; }
-      panic!("Could not find SDL2 via pkgconfig");
+      panic!("Could not find Tcl via pkgconfig");
     } else {
-      println!("cargo:rustc-flags=-l tcl");
+      println!("cargo:rustc-flags=-l tcl8.5");
     }
 }
 
 fn build_pkgconfig() -> bool {
-    let opts = pkg_config::default_options("tcl");
-    pkg_config::find_library_opts("tcl", &opts).is_ok()
+    let opts = pkg_config::default_options("tcl8.5");
+    pkg_config::find_library_opts("tcl8.5", &opts).is_ok()
 }
