1dd83323ad1bfda01ff6f89cf9fba90a5fc9d50d
Tony Aldridge <tony@dabapps.com>
2015-02-17 17:37:41 +0000

    Objects are now bound to a Tcl lifetime

--- src/object.rs
+++ src/object.rs
@@ -1,15 +1,18 @@
 use std::ffi::{CString, c_str_to_bytes};
 
 use ll::*;
+use tcl::TclEnvironment;
 
-pub struct Object {
+pub struct Object <'env> {
+    _env: &'env TclEnvironment,
     raw: *mut Tcl_Obj
 }
 
-impl Object {
+impl <'env> Object <'env> {
 
-    pub fn new() -> Object {
+    pub fn new(env: &TclEnvironment) -> Object {
         Object {
+            _env: env,
             raw: unsafe {
                 let raw = Tcl_NewObj();
                 Tcl_IncrRefCount(raw);
@@ -18,8 +21,9 @@ impl Object {
         }
     }
 
-    pub fn new_boolean(val: bool) -> Object {
+    pub fn new_boolean(env: &TclEnvironment, val: bool) -> Object {
         Object {
+            _env: env,
             raw: unsafe {
                 let raw = Tcl_NewBooleanObj(if val { 1 } else { 0 });
                 Tcl_IncrRefCount(raw);
@@ -28,8 +32,9 @@ impl Object {
         }
     }
 
-    pub fn new_integer(val: i32) -> Object {
+    pub fn new_integer(env: &TclEnvironment, val: i32) -> Object {
         Object {
+            _env: env,
             raw: unsafe {
                 let raw = Tcl_NewIntObj(val);
                 Tcl_IncrRefCount(raw);
@@ -38,8 +43,9 @@ impl Object {
         }
     }
 
-    pub fn new_long(val: i64) -> Object {
+    pub fn new_long(env: &TclEnvironment, val: i64) -> Object {
         Object {
+            _env: env,
             raw: unsafe {
                 let raw = Tcl_NewLongObj(val);
                 Tcl_IncrRefCount(raw);
@@ -51,8 +57,9 @@ impl Object {
     //TODO: WideInt
     //TODO: BigNum
 
-    pub fn new_double(val: f64) -> Object {
+    pub fn new_double(env: &TclEnvironment, val: f64) -> Object {
         Object {
+            _env: env,
             raw: unsafe {
                 let raw = Tcl_NewDoubleObj(val);
                 Tcl_IncrRefCount(raw);
@@ -61,9 +68,10 @@ impl Object {
         }
     }
 
-    pub fn new_string(val: &str) -> Object {
+    pub fn new_string(env: &'env TclEnvironment, val: &str) -> Object<'env> {
         let buf = CString::from_slice(val.as_bytes()).as_ptr();
         Object {
+            _env: env,
             raw: unsafe {
                 let raw = Tcl_NewStringObj(buf, val.len() as i32);
                 Tcl_IncrRefCount(raw);
@@ -72,8 +80,9 @@ impl Object {
         }
     }
 
-    pub fn new_byte_array(val: &[u8]) -> Object {
+    pub fn new_byte_array(env: &'env TclEnvironment, val: &[u8]) -> Object<'env> {
         Object {
+            _env: env,
             raw: unsafe {
                 let raw = Tcl_NewByteArrayObj(val.as_ptr(), val.len() as i32);
                 Tcl_IncrRefCount(raw);
@@ -135,7 +144,8 @@ impl Object {
     }
 }
 
-impl Drop for Object {
+#[unsafe_destructor]
+impl <'env> Drop for Object <'env> {
     fn drop(&mut self) {
         unsafe { Tcl_DecrRefCount(self.raw) };
     }
