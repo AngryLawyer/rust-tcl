b5009b987efd367df69585054e79a02d08056c1a
Tony Aldridge <tony@dabapps.com>
2015-02-23 14:00:15 +0000

    Starting to document

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
 #![feature(std_misc, path, env, unsafe_destructor, core)]
+
+#![crate_type= "lib"]
+
 extern crate "rust-tcl-sys" as ll;
 #[macro_use]
 extern crate bitflags;
 
+#[doc(no_inline)]
 pub use tcl::*;
+#[doc(no_inline)]
 pub use object::*;
+#[doc(no_inline)]
 pub use interpreter::*;
 
 pub mod tcl;
--- src/tcl.rs
+++ src/tcl.rs
@@ -9,8 +9,11 @@ use object::Object;
 
 static INIT_TCL: sync::Once = sync::ONCE_INIT;
 
+/// Acts as proof that Tcl has been correctly initialized for other parts of the code,
+/// and wraps all code that must 
 pub struct TclEnvironment;
 
+/// Initialize Tcl
 pub fn init() -> TclEnvironment {
     let ptr = match args().next() {
         Some(path) => {
@@ -28,22 +31,27 @@ pub fn init() -> TclEnvironment {
 
 impl TclEnvironment {
 
+    /// Create a new Tcl interpreter
     pub fn interpreter(&self) -> Interpreter {
        Interpreter::new(self)
     }
 
+    /// Create an untyped Tcl value
     pub fn object(&self) -> Object {
        Object::new(self)
     }
 
+    /// Create a Boolean Tcl value
     pub fn boolean(&self, val: bool) -> Object {
        Object::new_boolean(self, val)
     }
 
+    /// Create an Integer Tcl value
     pub fn integer(&self, val: i32) -> Object {
        Object::new_integer(self, val)
     }
 
+    /// Create a Long Tcl value
     pub fn long(&self, val: i64) -> Object {
        Object::new_long(self, val)
     }
@@ -51,29 +59,40 @@ impl TclEnvironment {
     //TODO: WideInt
     //TODO: BigNum
 
+
+    /// Create a Double Tcl value
     pub fn double(&self, val: f64) -> Object {
        Object::new_double(self, val)
     }
 
+    /// Create a String Tcl value
     pub fn string(&self, val: &str) -> Object {
         Object::new_string(self, val)
     }
 
+    /// Create a Byte Array Tcl value
     pub fn byte_array(&self, val: &[u8]) -> Object {
         Object::new_byte_array(self, val)
     }
 }
 
+/// TclResult represents the possible states an interpreter can be in after an evaluation
 #[derive(Debug)]
 pub enum TclResult {
+    /// The command completed successfully
     Ok,
+    /// There was an error
     Error(String),
+    /// The last command was a Return from a function
     Return,
+    /// The last command was a Break from a loop
     Break,
+    /// The last command was a Continue from a loop
     Continue
 }
 
 impl TclResult {
+    /// Convert a low-level representation of a Tcl result into a TclResult enum instance
     pub fn from_ll(result: i32, interpreter: &Interpreter) -> TclResult {
         match result {
             0 => TclResult::Ok,
