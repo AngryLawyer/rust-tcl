e5ce25d875885ba0a29c1b3105551f11e4e4debc
Tony Aldridge <tony@dabapps.com>
2015-03-02 14:50:29 +0000

    Finally corrected docs locations

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['tcl'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='tcl/tcl/enum.TclResult.html' title='tcl::tcl::TclResult'>TclResult</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['tcl'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'env&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='tcl/object/struct.Object.html' title='tcl::object::Object'>Object</a>&lt;'env&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;'env&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='tcl/interpreter/struct.Interpreter.html' title='tcl::interpreter::Interpreter'>Interpreter</a>&lt;'env&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
