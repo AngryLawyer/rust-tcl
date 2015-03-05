f49e9692f7851c7b3327a8f86fd5bb35d5f22438
Tony Aldridge <tony@dabapps.com>
2015-03-05 13:41:25 +0000

    Example repl now handles multiline plus escaping

--- examples/repl.rs
+++ examples/repl.rs
@@ -1,8 +1,102 @@
-#![feature(io)]
+#![feature(io, core)]
 
 extern crate tcl;
 use std::io;
-use std::io::{BufRead, Write};
+use std::io::BufRead;
+
+enum QuoteType {
+    None,
+    Bracket,
+    Quote
+}
+
+/// A class to consume input and make sure brackets/quotes match.
+struct LineAccumulator {
+    items: Vec<String>,
+    quote_type: QuoteType,
+    quote_depth: u32
+}
+
+impl LineAccumulator {
+
+    pub fn new() -> LineAccumulator {
+        LineAccumulator {
+            items: vec![],
+            quote_type: QuoteType::None,
+            quote_depth: 0
+        }
+    }
+
+    pub fn consume(&mut self, string: String) -> Option<String> {
+        let mut escaped = false;
+
+        for c in string.as_slice().chars() {
+            if escaped {
+                escaped = false;
+            } else if c == '\\' {
+                escaped = true;
+            } else {
+                match self.quote_type {
+                    QuoteType::None => {
+                        if c == '"' {
+                            self.quote_type = QuoteType::Quote;
+                        } else if c == '{' {
+                            self.quote_type = QuoteType::Bracket;
+                            self.quote_depth = 1;
+                        }
+                    },
+                    QuoteType::Bracket => {
+                        if c == '{' {
+                            self.quote_depth += 1;
+                        } else if c == '}' {
+                            self.quote_depth -= 1;
+                        }
+
+                        if self.quote_depth == 0 {
+                            self.quote_type = QuoteType::None;
+                        }
+                    },
+                    QuoteType::Quote => {
+                        if c == '"' {
+                            self.quote_type = QuoteType::None;
+                        }
+                    }
+                }
+            }
+        };
+
+        self.items.push(string);
+
+        match self.quote_type {
+            QuoteType::None => {
+                Some(self.items.connect(" "))
+            },
+            _ => None
+        }
+    }
+}
+
+fn get_input(input: &io::Stdin) -> String {
+    let mut in_lock = input.lock();
+    let mut accumulator = LineAccumulator::new();
+
+    let command;
+
+    loop {
+        let mut buf = String::new();
+        in_lock.read_line(&mut buf).ok().expect("Couldn't read stdin!");
+
+        match accumulator.consume(buf) {
+            Some(string) => {
+                command = string;
+                break;
+            },
+            None => ()
+        };
+    };
+
+    command
+}
 
 /**
  * A simple program that passes commands directly to the interpreter
@@ -17,13 +111,8 @@ pub fn main() {
     let mut interp = env.interpreter();
 
     loop {
-        let mut buf = String::new();
-        {
-            let mut in_lock = input.lock();
-            in_lock.read_line(&mut buf).ok().expect("Couldn't read stdin!");
-        }
-
-        let result = interp.eval(buf.as_slice(), tcl::EvalScope::Local);
+        let command = get_input(&input);
+        let result = interp.eval(command.as_slice(), tcl::EvalScope::Local);
 
         match result {
             tcl::TclResult::Error(string) => {
